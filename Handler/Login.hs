370f044f766cb24e80bac654d336690e301c19a7
Peter Harpending <peter@harpending.org>
2015-08-11 11:55:47 -0600

    Add faux login capability

--- Handler/Login.hs
+++ Handler/Login.hs
@@ -1,9 +1,48 @@
 module Handler.Login where
 
 import Import
+import Model.User
 
 getLoginR :: Handler Html
-getLoginR = defaultLayout $ do $(widgetFile "login")
+getLoginR = do
+  (widget, enctype) <- generateFormPost loginForm
+  defaultLayout $ do
+    [whamlet|
+      <form method=post
+            target=@{LoginR}
+            enctype=#{enctype}
+            role=form>
+        ^{widget}
+    |]
 
 postLoginR :: Handler Html
-postLoginR = error "Not yet implemented: postLoginR"
+postLoginR = do
+  ((result, widget), enctype) <- runFormPost loginForm
+  case result of
+    FormMissing ->
+      defaultLayout $ do
+        [whamlet|
+          <p>The form was missing. Please try again
+          <form method=post
+                target=@{LoginR}
+                enctype=#{enctype}
+                role=form>
+            ^{widget}
+        |]
+    FormFailure errors ->
+      defaultLayout $ do
+        [whamlet|
+          <p>There were some errors with the form data you submitted:
+          <ul>
+            $forall err <- errors
+              <li>#{err}
+          <form method=post
+                target=@{LoginR}
+                enctype=#{enctype}
+                role=form>
+            ^{widget}
+        |]
+    FormSuccess _ ->
+      error "Not yet implemented: logging in."
+      
+
